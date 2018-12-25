dfe3fd420ac81c951408fca3f29e6dfd572c1409
zhuhui <zhuhui@lonlife.cn>
2018-12-25 17:54:20 +0800

    添加json-server

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -1,29 +1,26 @@
-import {join} from 'lodash';
+import React from 'react';
 import './style.css';
-import myImage from './image.png';
-// import PrientMe from './prientMe';
 
-function component(){
-    var ele=document.createElement('div');
-    var btn= document.createElement('button');
-
-    ele.innerHTML = join(['Hello', 'webpack'], ' ');
-    ele.innerHTML='sadasda1343';
-    ele.classList.add('hello');
-
-    var image= new Image();
-    image.src=myImage;
-    image.width=700;
-    ele.appendChild(image);
-
-    btn.onclick=(e)=>import('./prientMe.js').then(module=>{
-        const PrientMe = module.default;
-        PrientMe();
-    });
-    btn.innerHTML='Click me and check the console!';
-    ele.appendChild(btn);
-
-    return ele;
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            count:1
+        }
+    }
+    handleClick=()=>{
+        this.setState(prevState=>{
+            return {
+                count:++prevState.count
+            }
+        })
+    }
+    render() {
+        const {count}=this.state;
+        return (
+            <h1 onClick={this.handleClick}>点击{count}次</h1>
+        )
+    }
 }
 
-export default component;
+export default App;
